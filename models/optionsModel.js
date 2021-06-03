b7fe56d16e3aca86284d84c96edb4588ac2772b2
Dima <dchaika1990@gmail.com>
2021-06-03 17:11:25 +0300

    moved the server and create client

--- models/optionsModel.js
+++ models/optionsModel.js
@@ -26,6 +26,8 @@ class optionsModel {
 		}
 	}
 
+	// asyns set
+
 	async getOneProductOption(id, callback) {
 		await DB.query('SELECT product_options.id_product, product_type.type_name, product_color.color_name, product_size.size_name FROM product_options, product_type, product_color, product_size where id_product=? and product_options.product_type=product_type.id_type and product_options.product_color=product_color.id_color and product_options.product_size=product_size.id_size', [id], result => {
 			callback(result);
--- models/productModel.js
+++ models/productModel.js
@@ -13,12 +13,6 @@ class ProductModel {
 		})
 	}
 
-	// async getOneProducts(id, callback) {
-	// 	await DB.query('SELECT * FROM products inner join structure on id_product=? and products.product_structure=structure.id_structure inner join product_category on products.id_product=product_category.id_product inner join category on product_category.id_category=category.id_category', [id], result => {
-	// 		callback(result);
-	// 	})
-	// }
-
 	async getOneProductOption(id, callback) {
 		await DB.query('SELECT product_options.id_product, product_type.type_name, product_color.color_name, product_size.size_name FROM product_options, product_type, product_color, product_size where id_product=? and product_options.product_type=product_type.id_type and product_options.product_color=product_color.id_color and product_options.product_size=product_size.id_size', [id], result => {
 			callback(result);
